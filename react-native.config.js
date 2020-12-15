eb7b6d1700f0287402f8372e3bf7b63a47054c65
258333601@163.com <258333601@163.com>
2020-12-16 00:07:09 +0400

    android 初始版本

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,23 @@
 module.exports = {
     dependencies: {
-        // 'react-native-code-push': {
-        //     platforms: {
-        //         android:null,
-        //     },
-        // },
+        'react-native-code-push': {
+            platforms: {
+                android:null,
+            },
+        },
+        'jshare-react-native' : {
+            platforms: {
+                android: {
+                    packageInstance: 'new JSharePackage(false, false)'
+                }
+            }
+        },
+        'jpush-react-native': {
+            platforms: {
+                android:null,
+            },
+        },
+
+
     },
 };
